fdd2c6cb6d8e9babb85fb2c2050a8eb4e2de1b46
adindacarissa06 <dindacarissa18@gmail.com>
2024-12-12 14:03:07 +0700

    Update api.dart

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -1,7 +1,7 @@
 class BaseUrl {
-  static String tambah = "http://192.168.100.40/biodata/create.php";
-  static String lihat = "http://192.168.100.40/biodata/details.php";
-  static String edit = "http://192.168.100.40/biodata/update.php";
-  static String hapus = "http://192.168.100.40/biodata/delete.php";
-  static String data = "http://192.168.100.40/biodata/list.php";
+  static String tambah = "http://192.168.100.48/projectbaru/create.php";
+  static String lihat = "http://192.168.100.48/projectbaru/details.php";
+  static String edit = "http://192.168.100.48/projectbaru/update.php";
+  static String hapus = "http://192.168.100.48/projectbaru/delete.php";
+  static String data = "http://192.168.100.48/projectbaru/list.php";
 }
